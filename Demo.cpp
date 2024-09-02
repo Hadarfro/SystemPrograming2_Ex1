131ccc2c4ee167413b46c609cceb5cf89481d9cd
Hadar Froimowich <122720878+Hadarfro@users.noreply.github.com>
2024-09-02 17:17:20 +0300

    Update

--- Demo.cpp
+++ Demo.cpp
@@ -1,11 +1,8 @@
 /*
- * Demo program for Exercise 2.
- * Author: Benjamin Saldman.
+ * Author: Hadar Froimowich.
+ * ID: 213118458
+ * Email: hadarfro12@gmail.com
  */
-
-#include "Graph.hpp"
-#include "Algorithms.cpp"
-using namespace Algorithms;
 
 #include <iostream>
 #include <stdexcept>
@@ -14,22 +11,24 @@ using namespace Algorithms;
 #include "Algorithms.hpp"
 using namespace std;
 using namespace ariel;
+using namespace Algorithms;
 
 int main()
 {
     ariel::graph g;
-    // 3x3 matrix that represents a connected graph.
-    vector<vector<int>> graph = {
+     // 3x3 matrix that represents a connected graph.
+    vector<vector<int>> graph1 = {
         {0, 1, 0},
         {1, 0, 1},
         {0, 1, 0}};
-    g.loadGraph(graph); // Load the graph to the object.
+    g.loadGraph(graph1); // Load the graph to the object.
 
-    g.printGraph();                                    // Should print: "Graph with 3 vertices and 4 edges."
-    cout << Algorithms::isConnected(g) << endl;        // Should print: "1" (true).
-    cout << Algorithms::shortestPath(g, 0, 2) << endl; // Should print: 0->1->2.
-    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "0" (false).
-    cout << Algorithms::isBipartite(g) << endl;        // Should print: "The graph is bipartite: A={0, 2}, B={1}."
+    g.printGraph();                    // Should print: "Graph with 3 vertices and 3 edges."
+    Algorithms::isConnected(g);        // Should print: "1" (true).
+    Algorithms::shortestPath(g, 0, 2); // Should print: "0->1->2".
+    Algorithms::isContainsCycle(g);    // Should print: "0" (false).
+    Algorithms::isBipartite(g);        // Should print: "The graph is bipartite: A={0, 2}, B={1}".
+    Algorithms::negativeCycle(g);
 
     // 5x5 matrix that represents a non-connected graph with a cycle.
     vector<vector<int>> graph2 = {
@@ -42,12 +41,12 @@ int main()
     g.loadGraph(graph2); // Load the graph to the object.
 
     g.printGraph();                                    // Should print: "Graph with 5 vertices and 8 edges."
-    cout << Algorithms::isConnected(g) << endl;        // Should print: "0" (false).
-    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: "-1" (there is no path between 0 and 4).
-    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "The cycle is: 0->1->2->0".
-    cout << Algorithms::isBipartite(g) << endl;        // Should print: "0" (false).
+    Algorithms::isConnected(g);        // Should print: "0" (false).
+    Algorithms::shortestPath(g, 0, 4); // Should print: "-1" (there is no path between 0 and 4).
+    Algorithms::isContainsCycle(g);    // Should print: "The cycle is: 0->1->2->0".
+    Algorithms::isBipartite(g);        // Should print: "0" (false).
 
-    // 5x5 matrix that reprsents a connected weighted graph.
+    // 5x5 matrix that represents a connected weighted graph.
     vector<vector<int>> graph3 = {
         {0, 1, 2, 0, 0},
         {1, 0, 3, 0, 0},
@@ -57,12 +56,13 @@ int main()
     g.loadGraph(graph3); // Load the graph to the object.
 
     g.printGraph();                                    // Should print: "Graph with 5 vertices and 10 edges."
-    cout << Algorithms::isConnected(g) << endl;        // Should print: "1" (true).
-    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: 0->2->3->4.
-    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "0" (false).
-    cout << Algorithms::isBipartite(g) << endl;        // Should print: "The graph is bipartite: A={0, 2, 4}, B={1, 3}."
+    Algorithms::isConnected(g) ;        // Should print: "1" (true).
+    Algorithms::shortestPath(g, 0, 4) ; // Should print: "0->2->3->4".
+    Algorithms::isContainsCycle(g);    // Should print: "0" (false).
+    Algorithms::isBipartite(g);        // Should print: "The graph is not bipartite"
+    
 
-    // 5x4 matrix that reprsents invalid graph.
+     // 5x4 matrix that represents an invalid graph.
     vector<vector<int>> graph4 = {
         {0, 1, 2, 0},
         {1, 0, 3, 0},
